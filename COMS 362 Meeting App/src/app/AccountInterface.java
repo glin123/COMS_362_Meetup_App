7810f04bd2b19e3d98c1858811ba2e090df8d772
Clayton Nida <canida2@iastate.edu>
2019-03-13 19:34:35 -0500

    formatting

--- COMS 362 Meeting App/src/app/AccountInterface.java
+++ COMS 362 Meeting App/src/app/AccountInterface.java
@@ -1,10 +1,16 @@
 package app;
 
-public interface AccountInterface {
+public interface AccountInterface
+{
 	void setUsername(String input);
+
 	String getUsername();
+
 	void setPassword(String input);
+
 	String getPassword();
+
 	void setEmail(String input);
+
 	String getEmail();
 }
